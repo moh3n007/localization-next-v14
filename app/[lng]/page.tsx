73c3217a30459d408a404273cfc95e54c3cb88d4
moh3n007 <lotfimohsen007@gmail.com>
2024-04-11 16:20:01 +0300

    feat: dictionary is added

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -22,9 +22,9 @@ export default async function Home({
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            {dict.home.by}{" "}
             <Image
-              src="/vercel.svg"
+              src="/next_static/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -38,7 +38,7 @@ export default async function Home({
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src="/next_static/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
@@ -54,13 +54,13 @@ export default async function Home({
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+            {dict.home.ducs}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            {dict.home.ducs_description}
           </p>
         </a>
 
@@ -71,13 +71,13 @@ export default async function Home({
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+            {dict.home.learn}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            {dict.home.learn_description}
           </p>
         </a>
 
@@ -88,13 +88,13 @@ export default async function Home({
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
+            {dict.home.templates}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            {dict.home.templates_description}
           </p>
         </a>
 
@@ -105,13 +105,13 @@ export default async function Home({
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+            {dict.home.deploy}{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            {dict.home.deploy_description}
           </p>
         </a>
       </div>
